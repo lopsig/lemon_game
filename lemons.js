f8e8eb0f3c9a5b7aabe4e25146547813d9ee437d
lopsig <jonathan.ls.trabajo@gmail.com>
2026-04-11 21:54:39 -0500

    add move left function and move right function

--- lemons.js
+++ lemons.js
@@ -1,10 +1,15 @@
 let canvas = document.getElementById("gameArea")
 let context = canvas.getContext("2d")
 
+///CONSTANTES///
 const FLOOR_HEIGHT = 20;
 const CHARACTER_HEIGHT = 60
 const CHARACTER_WIDTH = 40
 
+///VARIABLES///
+let characterX = canvas.width / 2;
+
+///FUNCIONES///
 function start() {
   drawFloor();
   drawCharacter();
@@ -19,6 +24,25 @@ const drawFloor = () => {
 
 const drawCharacter = () => {
   context.fillStyle = "black";
-  context.fillRect(canvas.width/2-FLOOR_HEIGHT, canvas.height-CHARACTER_HEIGHT-FLOOR_HEIGHT, CHARACTER_WIDTH, CHARACTER_HEIGHT)
+  context.fillRect(characterX-CHARACTER_WIDTH/2, canvas.height-CHARACTER_HEIGHT-FLOOR_HEIGHT, CHARACTER_WIDTH, CHARACTER_HEIGHT)
 
+}
+
+const moveLeft = () => {
+  characterX = characterX - 5;
+  updateScreen()
+}
+const moveRight = () => {
+  characterX = characterX + 5;
+  updateScreen()
+}
+
+const updateScreen = () => {
+  clearCanva();
+  drawCharacter();
+  
+}
+
+const clearCanva = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height - FLOOR_HEIGHT)
 }
