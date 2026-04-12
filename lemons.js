3dd8aff39263b7d40b9e2976f036909ac0f5d42c
lopsig <jonathan.ls.trabajo@gmail.com>
2026-04-11 21:20:28 -0500

    add drawFloor function and drawCharacter function

--- lemons.js
+++ lemons.js
@@ -1,2 +1,24 @@
 let canvas = document.getElementById("gameArea")
 let context = canvas.getContext("2d")
+
+const FLOOR_HEIGHT = 20;
+const CHARACTER_HEIGHT = 60
+const CHARACTER_WIDTH = 40
+
+function start() {
+  drawFloor();
+  drawCharacter();
+  
+}
+
+const drawFloor = () => {
+  context.fillStyle = "brown";
+  context.fillRect(0, canvas.height-FLOOR_HEIGHT, canvas.width, FLOOR_HEIGHT)
+  
+}
+
+const drawCharacter = () => {
+  context.fillStyle = "black";
+  context.fillRect(canvas.width/2-FLOOR_HEIGHT, canvas.height-CHARACTER_HEIGHT-FLOOR_HEIGHT, CHARACTER_WIDTH, CHARACTER_HEIGHT)
+
+}
